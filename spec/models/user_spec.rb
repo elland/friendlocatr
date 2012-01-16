b722fdecefd3313c28c7edbedf0523c88d80d642
Igor Ranieri Elland <eu@elland.me>
2012-01-16 15:31:55 -0200

    Better name validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,5 +4,6 @@ describe User do
   it {should validate_presence_of(:name) }
   it {should ensure_inclusion_of(:lat).in_range(0..90) }
   it {should ensure_inclusion_of(:lon).in_range(0..180) }
-  it {should validate_length_of(:name).within(0..50) }
+  it {should allow_value('Valid name').for(:name) }
+  it {should_not allow_value('This is most certainly an invalid name, I am sure of it!').for(:name) }
 end
