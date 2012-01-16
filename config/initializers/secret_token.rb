7aa7f951af28292a92c4545b7674805992191e17
Igor Ranieri Elland <eu@elland.me>
2012-01-16 14:36:42 -0200

    Add test gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Friendlocatr::Application.config.secret_token = 'db9b2c69b98edd286486c7c4c8c1e2e79c539e44c1529152648706c28decd33cc8e647a24d3589b37f14c498c825c6a29cb30e3856dd43fd34e9b5d8a09e8650'
